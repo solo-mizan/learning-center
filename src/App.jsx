381a240928a6ebb19c024a80b364bfa0c1905477
solo-mizan <mizanbd.eee@gmail.com>
2024-01-16 15:42:27 +0600

    student section and footer are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Footer from "./components/footer";
 import HeroSection from "./components/hero-section";
 import Navbar from "./components/navbar";
-import StudentsTable from "./components/students-table";
+import StudentsSection from "./components/students-section";
 
 function App() {
   return (
     <>
       <div className="bg-[#172227] font-[Inter] text-white">
-        <Navbar />
+        <Navbar />  
         <HeroSection />
-        <StudentsTable />
+        <StudentsSection />
         <Footer />
       </div>
     </>
